05334121b58c3856b2eb6e1642e37815b6bdff07
MarkWaldron <markewaldron@gmail.com>
2015-03-17 18:00:21 -0400

    Updated user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,4 @@
 class User < ActiveRecord::Base
-	attr_accessible :name, :username, :password, :email
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
